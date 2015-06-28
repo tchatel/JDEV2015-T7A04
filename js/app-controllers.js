023b98a0920d37ab3666cd5dc492e7e11d2fc9a2
Thierry Chatel <tchatel@gmail.com>
2015-06-28 17:22:26 +0200

    étape A1

--- js/app-controllers.js
+++ js/app-controllers.js
@@ -2,7 +2,15 @@
 
 angular.module('app-controllers', [])
 
-    .controller('CatalogController', function ($scope) {
+    .controller('CatalogController', function ($scope, $http) {
+        $http.get('/data/catalog.json')
+            .success(function(data, status, headers, config) {
+                $scope.books = data;
+            })
+            .error(function(data, status, headers, config) {
+                console.log("HTTP Error: ", data, status, headers, config);
+            });
+
     })
 
 ;
