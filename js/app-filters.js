94f48c9f9855b232a683b2de8340bf14ccd6c2e4
Thierry Chatel <tchatel@gmail.com>
2015-06-28 18:14:53 +0200

    étape B2

--- js/app-filters.js
+++ js/app-filters.js
@@ -2,4 +2,16 @@
 
 angular.module('app-filters', [])
 
+    .filter('range', function ($parse) {
+        return function(array,expression, min, max) {
+            var getter = $parse(expression);
+            return array && array.filter(function (item) {
+                var value = getter(item),
+                    okMin = !min || value >= min,
+                    okMax = !max || value <= max;
+                return okMin && okMax;
+            });
+        };
+    })
+
 ;
