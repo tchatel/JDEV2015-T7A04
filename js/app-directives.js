ba5e66a4c43d80f2c91465d84a3d914c23889b5d
Thierry Chatel <tchatel@gmail.com>
2015-06-28 19:20:26 +0200

    étape D1

--- js/app-directives.js
+++ js/app-directives.js
@@ -2,4 +2,20 @@
 
 angular.module('app-directives', [])
 
+    .directive('rating', function () {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var img = '<img src="img/star.png"/>',
+                    html = '';
+                scope.$watch(attrs.rating, function (value) {
+                    for (var i = 0 ; i < value ; i++) {
+                        html += img;
+                    }
+                    element.html(html);
+                });
+            }
+        };
+    })
+
 ;
